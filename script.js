2188c656012d4a05288cfd962f879eb3d3bf1841
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-08 11:22:09 +0530

    pokemon will have statistics instead description
    What changes you have made?
    1. Modified script
        now it will give ststistics of pokemon instead of description
    2. generaed new code with script and changed in index.html
    3. added icon in title
    4. modified index.html added css for ststistics

--- script.js
+++ script.js
@@ -7,27 +7,39 @@ const html = [
 
   '</h3>\n<img src=',
 
-  ' alt=',
+  'alt=',
 
-  '/>\n<p>\n',
+  '/>\n<h3 class="stats">STATISTICS</h3>\n<div class="type">\n',
 
-  '\n</p>\n</div>'
+  '\n</div>\n</div>'
 ];
 
-const getHtml = () => {
+const getId = id => {
+  if (id < 10) return `#00${id}`;
+  if (id < 100) return `#0${id}`;
+  return `#${id}`;
+};
+
+const getHtml = (pokemonData, filePath) => {
   pokemonData.forEach(pokemon => {
+    const types = pokemon.type.reduce((string, info) => {
+      string += `<p>${info}</p>`;
+      return string;
+    }, '');
     const code =
       html[0] +
+      getId(pokemon.id) +
+      ' ' +
       pokemon.name +
       html[1] +
       `"${pokemon.art_url}"` +
       html[2] +
       `"${pokemon.name}Image"` +
       html[3] +
-      pokemon.description +
+      types +
       html[4];
-    appendFileSync('./generatedHtml.html', code, 'utf8');
+    appendFileSync(filePath, code, 'utf8');
   });
 };
 
-getHtml();
+getHtml(pokemonData, './generatedHtml.html');
