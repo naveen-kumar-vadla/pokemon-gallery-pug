ac74b517b5d8590aa8ca82cef1bc600e0bd80fa2
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-10 09:32:46 +0530

    modified searchPokemon and pokemon card color
    What changes you have made?
    1. Modified search box placeholder text
    2. modified card background color
    3. modified searchPokemon

--- searchPokemon.js
+++ searchPokemon.js
@@ -1,13 +1,17 @@
-const filterPokemons = (pokemon, text) => {
+const filterPokemon = (pokemon, text) => {
   const pokemonName = pokemon.innerText.toLowerCase();
   return pokemonName.includes(text);
 };
+const displayPokemon = text => {
+  const allPokemon = Array.from(document.getElementsByClassName('pokemon'));
+  allPokemon.forEach(pokemon => (pokemon.style['display'] = 'none'));
+  const filteredPokemon = allPokemon.filter(pokemon =>
+    filterPokemon(pokemon, text)
+  );
+  filteredPokemon.forEach(pokemon => (pokemon.style['display'] = 'flex'));
+};
+
 const searchPokemon = () => {
   const text = document.getElementById('searchBox').value.toLowerCase();
-  const allPokemons = Array.from(document.getElementsByClassName('pokemon'));
-  allPokemons.forEach(pokemon => (pokemon.style['display'] = 'none'));
-  const filteredPokemons = allPokemons.filter(pokemon =>
-    filterPokemons(pokemon, text)
-  );
-  filteredPokemons.forEach(pokemon => (pokemon.style['display'] = 'flex'));
+  displayPokemon(text);
 };
