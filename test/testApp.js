908ee0b9be356d709dc6eb259b955663b5ff36bf
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-19 20:45:16 +0530

    added tests for GET and POST FILE NOT FOUND and METHOD NOT ALLOWED

---
+++ test/testApp.js
@@ -0,0 +1,52 @@
+'use strict';
+const request = require('supertest');
+
+const app = require('../lib/app');
+
+describe('GET', () => {
+  describe('FILE NOT FOUND', () => {
+    it('Should give file not found if file not exist', done => {
+      request(app)
+        .get('/badFile')
+        .set('Accept', '*/*')
+        .expect(404)
+        .expect('Content-Type', /html/)
+        .expect('Content-Length', '146')
+        .expect(/\/badFile/, done);
+    });
+  });
+});
+describe('POST', () => {
+  describe('FILE NOT FOUND', () => {
+    it('Should give file not found if file not exist', done => {
+      request(app)
+        .post('/badFile')
+        .set('Accept', '*/*')
+        .send('{ "name":"raja", "comment":"wonderful+site" }')
+        .expect(404)
+        .expect('Content-Type', /html/)
+        .expect('Content-Length', '147')
+        .expect(/\/badFile/, done);
+    });
+  });
+});
+describe('METHOD NOT ALLOWED', () => {
+  it('Should should give method not allowed for put method ', done => {
+    request(app)
+      .put('/')
+      .set('Accept', '*/*')
+      .expect(404)
+      .expect('Content-Type', /html/)
+      .expect('Content-Length', '139')
+      .expect(/Cannot PUT/, done);
+  });
+  it('Should should give method not allowed for delete method ', done => {
+    request(app)
+      .delete('/')
+      .set('Accept', '*/*')
+      .expect(404)
+      .expect('Content-Type', /html/)
+      .expect('Content-Length', '142')
+      .expect(/Cannot DELETE/, done);
+  });
+});
